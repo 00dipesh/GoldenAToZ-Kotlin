14b3933f1b25be15f6c85a4724f6d4af8c227cb1
suparna72 <suparnabundhate72@gmail.com>
2024-03-22 15:13:18 +0530

    add splashscreen on 220324

---
+++ app/src/main/java/com/goldendigitech/goldenatoz/singleToneClass/SharedPreferencesManager.kt
@@ -0,0 +1,31 @@
+package com.goldendigitech.goldenatoz.singleToneClass
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class SharedPreferencesManager private constructor(context: Context) {
+
+    private val sharedPreferences: SharedPreferences =
+        context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+
+    companion object {
+        private const val PREFS_NAME = "MySharedPreferences"
+        private var instance: SharedPreferencesManager? = null
+
+        @Synchronized
+        fun getInstance(context: Context): SharedPreferencesManager {
+            if (instance == null) {
+                instance = SharedPreferencesManager(context.applicationContext)
+            }
+            return instance!!
+        }
+    }
+
+    fun saveUserId(userId: Int) {
+        sharedPreferences.edit().putInt("userId", userId).apply()
+    }
+
+    fun getUserId(): Int {
+        return sharedPreferences.getInt("userId", 0)
+    }
+}
---
+++ app/src/main/java/com/goldendigitech/goldenatoz/splash/SplashScreen.kt
@@ -0,0 +1,49 @@
+package com.goldendigitech.goldenatoz.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.preference.PreferenceManager
+import android.util.Log
+import com.goldendigitech.goldenatoz.MainActivity
+import com.goldendigitech.goldenatoz.R
+import com.goldendigitech.goldenatoz.singleToneClass.SharedPreferencesManager
+
+class SplashScreen : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "SplashScreen"
+    }
+
+    private lateinit var sharedPreferencesManager: SharedPreferencesManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        // Initialize SharedPreferencesManager
+        sharedPreferencesManager = SharedPreferencesManager.getInstance(this)
+
+
+        val userId = sharedPreferencesManager.getUserId()  // Retrieve user ID
+        Log.d(TAG, "Splash userid: $userId")
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            if (userId == 0) {
+                // If user ID is not found, navigate to login activity
+                Log.d(TAG, "User ID not found in SharedPreferences")
+                val intent = Intent(this, LoginPasswordActivity::class.java)
+                startActivity(intent)
+            } else {
+                // If user ID is found, navigate to main activity
+                Log.d(TAG, "User ID found: $userId")
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+            finish()
+        }, 2000)
+    }
+}
