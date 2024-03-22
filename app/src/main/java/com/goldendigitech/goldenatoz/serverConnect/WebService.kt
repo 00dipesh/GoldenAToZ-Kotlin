385da916f2a43941fbbc53cf36de6d567991c180
suparna72 <suparnabundhate72@gmail.com>
2024-03-22 12:47:19 +0530

    add packages & retrofit dependancy on 220321

---
+++ app/src/main/java/com/goldendigitech/goldenatoz/serverConnect/WebService.kt
@@ -0,0 +1,14 @@
+package com.goldendigitech.goldenatoz.serverConnect
+
+interface WebService {
+
+//    @POST("Api/Account/Register")
+//    fun registerOrganisation(@Body registerModel: RegisterModel): Call<RegisterResponce>
+
+//    @GET("Api/Employee/Get")
+//    fun getEmployee(@Query("employeeId") employeeId: Int?): Call<GetEmployeeModel>
+//
+//    @POST("Api/Employee/Login")
+//    fun userLogin(@Body loginModel: LoginModel): Call<LoginResponse>
+
+}
