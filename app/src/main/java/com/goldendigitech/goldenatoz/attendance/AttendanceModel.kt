a9f86675b2a9af40a62852edb9689da1da214433
suparna72 <suparnabundhate72@gmail.com>
2024-03-29 11:36:51 +0530

    complete Attendance module on 290324

--- app/src/main/java/com/goldendigitech/goldenatoz/attendance/AttendanceModel.kt
+++ app/src/main/java/com/goldendigitech/goldenatoz/attendance/AttendanceModel.kt
@@ -1,28 +1,21 @@
 package com.goldendigitech.goldenatoz.attendance
 
-data class AttendanceModel (
-    val signInTime: String,
-    val signOutTime: String,
-    val employeeId: Int,
-    val employeeName: String,
-    val hq: String,
-    val employeeCode: String,
-    val workingStatus: String,
-    val workingFrom: String,
-    val typesOfLeave: String,
-    val fromDate: String,
-    val toDate: String,
-    val areaOfWork: String,
-    val fileName: String,
-    val fileType: String,
-    val fileContent: String,
-    val workingWith: String,
-    val seniorId: Int,
-    val juniorId: Int,
-    val taskOfTheDay: String,
-    val latitude: Double,
-    val longitude: Double,
-    val workingHr: String,
-    val remark: String,
-    val addressDetails: String
+import com.google.gson.annotations.SerializedName
+
+data class AttendanceModel(
+    @SerializedName("EmployeeId") val employeeId: Int,
+    @SerializedName("EmployeeCode") val employeeCode: String,
+    @SerializedName("WorkingStatus") val workingStatus: String,
+    @SerializedName("WorkingFrom") val workingFrom: String,
+    @SerializedName("AreaOfWork") val areaOfWork: String,
+    @SerializedName("FileName") val fileName: String,
+    @SerializedName("FileType") val fileType: String,
+    @SerializedName("FileContent") val fileContent: String,
+    @SerializedName("WorkingWith") val workingWith: String,
+    @SerializedName("SeniorId") val seniorId: Int?,
+    @SerializedName("JuniorId") val juniorId: Int?,
+    @SerializedName("TaskOfTheDay") val taskOfTheDay: String,
+    @SerializedName("Latitude") val latitude: Double,
+    @SerializedName("Longitude") val longitude: Double,
+    @SerializedName("Remark") val remark: String
 )
--- app/src/main/java/com/goldendigitech/goldenatoz/attendance/WorkingWithResponse.kt
+++ app/src/main/java/com/goldendigitech/goldenatoz/attendance/WorkingWithResponse.kt
@@ -2,7 +2,9 @@ package com.goldendigitech.goldenatoz.attendance
 
 import com.google.gson.annotations.SerializedName
 
-data class AttendanceResponse(
+data class WorkingWithResponse (
+
     @SerializedName("Success") val success: Boolean,
+    @SerializedName("Data") val data: Map<Int, String>, // Map of ID to name
     @SerializedName("Message") val message: String?
 )
